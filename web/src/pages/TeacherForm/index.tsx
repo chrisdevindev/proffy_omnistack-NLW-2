f5559b46407734bb6509b7ca7dea88ae4044e33c
Christian <alvescastro00cca13@gmail.com>
2020-08-04 03:26:02 -0300

    TeacherList screen

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import PageHeader from '../../components/PageHeader';
 
 function TeacherForm() {
     return(
-        <h1>Teacher Form</h1>
+        <div>
+           <div id="page-teacher-form" className="container">
+               <PageHeader title="Que incrível que você quer dar aulas."/>
+           </div>
+       </div>
     )
 }
 export default TeacherForm;
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import backIcon from '../../assets/images/icons/back.svg'
+import logoImg from '../../assets/images/logo.svg'
+
+import PageHeader from '../../components/PageHeader'
+import './style.css'
 
 function TeacherList() {
     return(
-        <h1>Teachers</h1>
+       <div>
+           <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponíveis.">
+               <form action="" id="search-teachers">
+                   <div className="input-block">
+                       <label htmlFor="subject">Matéria</label>
+                       <input type="text"id="subject" />
+                   </div>
+
+                   <div className="input-block">
+                       <label htmlFor="week_day">Dia da semana</label>
+                       <input type="text"id="week_day" />
+                   </div>
+
+                   <div className="input-block">
+                       <label htmlFor="time">Hora</label>
+                       <input type="text"id="time" />
+                   </div>
+               </form>
+            </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <img src="" alt=""/>
+                </article>
+            </main>
+           </div>
+       </div>
     )
 }
 export default TeacherList;
