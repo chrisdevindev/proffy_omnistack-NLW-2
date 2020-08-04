73058874ad0a11d65a7c62d9649113218d72f9e8
Christian <alvescastro00cca13@gmail.com>
2020-08-04 01:10:21 -0300

    Added Home-page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import './assets/styles/global.css'
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Routes/>
   );
 }
 
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function TeacherList() {
+    return(
+        <h1>Teachers</h1>
+    )
+}
+export default TeacherList;
