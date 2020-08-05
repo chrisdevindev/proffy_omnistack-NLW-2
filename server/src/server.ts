ed0f2bc11a69235af4ae156bb48f012b7fc023a4
Christian <alvescastro00cca13@gmail.com>
2020-08-05 20:07:30 -0300

    Creating class

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,10 @@
 import express from 'express'
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/users', ( request, response) => {
-
-    const users = [
-        {name: "chris", idade: 20},
-        {name: "chris", idade: 20}
-    ];
-
-    return response.json(users)
-})
+app.use(routes);
 
 app.listen(3333)
