1e3b8b54201f956ea387b0c1fbf45e3aedf77a69
Christian <alvescastro00cca13@gmail.com>
2020-08-11 17:50:35 -0300

    Removing header

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import Landing from './src/pages/Landing';
 import { AppLoading } from 'expo'
 
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo'
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins' 
+import AppStack from './src/routes/AppStack';
 
 export default function App() {
 
@@ -20,7 +20,7 @@ export default function App() {
   }else{
     return(
       <>
-        <Landing/>
+        <AppStack/>
         <StatusBar style="light"/>
       </>
     )
