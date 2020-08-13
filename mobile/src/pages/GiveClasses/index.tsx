3f94be1cc7e16ff20eadf25cce6f5065a0844424
Christian <alvescastro00cca13@gmail.com>
2020-08-13 00:19:52 -0300

    Page Dar Aulas

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react' 
-import { View, ImageBackground } from 'react-native'
+import { View, ImageBackground, Text } from 'react-native'
 import giveClassesbgImage from '../../assets/images/give-classes-background.png'
+import { RectButton } from 'react-native-gesture-handler'
 
 import styles from './styles'
+import { useNavigation } from '@react-navigation/native'
 
 function GiveClasses(){
+    const {goBack} = useNavigation()
+
+
+    function handleNavigateBack(){
+        goBack()
+    }
+
     return (
         <View style={styles.container}>
             <ImageBackground 
@@ -12,8 +21,19 @@ function GiveClasses(){
                 source={giveClassesbgImage} 
                 style={styles.content}
             >
+                <Text style={styles.title}>
+                    Quer ser um Proffy?
+                </Text>
+                
+                <Text style={styles.description}>
+                    Para começar você precisa se cadastrar como professor na nossa plataforma web.                
+                </Text>
 
             </ImageBackground>
+
+            <RectButton style={styles.okButton} onPress={handleNavigateBack}>
+                <Text style={styles.okButtonText}> Tudo jóia</Text>
+            </RectButton>
         </View> 
         
         
--- mobile/src/pages/GiveClasses/styles.ts
+++ mobile/src/pages/GiveClasses/styles.ts
@@ -11,6 +11,34 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
     },
+    title:{
+        fontFamily: 'Archivo_700Bold',
+        color: '#fff',
+        fontSize: 32,
+        lineHeight: 37,
+        maxWidth: 180,
+    },
+    description:{
+        marginTop: 24,
+        color: '#d4c2ff',
+        fontSize: 16,
+        lineHeight: 26,
+        fontFamily: 'Poppins_400Regular',
+        maxWidth: 240,
+    },
+    okButton:{
+        marginVertical: 40,
+        backgroundColor: '#04d361',
+        height: 58,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 8,
+    },
+    okButtonText:{
+        color: '#fff',
+        fontSize: 16,
+        fontFamily: 'Archivo_700Bold'
+    }
 })
 
 export default styles
