e440aa33efe173f563bc61002b8d4775ebcb13a2
Christian <alvescastro00cca13@gmail.com>
2020-08-13 13:09:08 -0300

    Component Page-Header

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import { View } from 'react-native'
 
 import styles from './styles'
+import PageHeader from '../../components/PageHeader'
 
 function TeacherList(){
-    return <View style={styles.container}/>
+    return (
+        <View style={styles.container}>
+            <PageHeader title="Proffys Disponíveis"/> 
+        </View>
+        
+    )
 }
 
 export default TeacherList
