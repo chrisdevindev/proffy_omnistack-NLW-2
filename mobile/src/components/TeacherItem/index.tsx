48349e7d923f5352e13b8e337c9ea9b2c9d122d2
Christian <alvescastro00cca13@gmail.com>
2020-08-17 21:32:33 -0300

    Counting connections

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -9,6 +9,7 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.png'
 
 import styles from './style'
 import { useAsyncStorage } from '@react-native-community/async-storage'
+import api from '../../services/api'
 
 export interface Teacher {
     id: number
@@ -31,6 +32,11 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
 
 
     function handleLinkToWhatsapp() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+
+
         Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
     }
 
